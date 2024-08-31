3e2c15bf4dbef4b58c3050d131dcd8aab5f314d9
ervanervan <ervanervan040@gmail.com>
2024-08-31 21:24:08 +0700

    update: dashboard page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const DashboardPage = () => {
   const [data, setData] = useState([]);
